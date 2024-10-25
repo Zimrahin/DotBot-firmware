21333baefc2e41645c7eb5092c2ae7d53f94d532
Zimrahin <diegobadillosj@gmail.com>
2024-10-25 15:52:16 +0200

    added config.py file with experiment variables

---
+++ a_python/config.py
@@ -0,0 +1,13 @@
+# Default values
+DEFAULT_USB_PORT = "/dev/ttyACM0"
+DEFAULT_BAUDRATE = 1000000
+
+
+FREQ_INTERFERER = 2450  # MHz
+TX_POWER_INTERFERER = 0  # dBm
+TX_POWER_LINK = 0  # dBm
+INTERFERER_MODE = "IEEE802154250Kbit"
+LENGTH_INTERFERER = 64  # Bytes
+
+tx_power_values = [8, 7, 6, 5, 4, 3, 2, 0, -4, -8, -12, -16, -20, -40]  # dBm
+radio_modes = ["BLE1MBit", "BLE2MBit", "BLELR125Kbit", "BLELR500Kbit", "IEEE802154250Kbit", "tone"]
--- a_python/serial_logger.py
+++ a_python/serial_logger.py
@@ -4,20 +4,16 @@ import jsonlines
 import argparse
 import sys
 from hdlc import HDLCHandler, HDLCState
-
-# Default values
-DEFAULT_USB_PORT = "/dev/ttyACM0"
-DEFAULT_BAUDRATE = 1000000
-
-
-FREQ_INTERFERER = 2450  # MHz
-TX_POWER_INTERFERER = 0  # dBm
-TX_POWER_LINK = 0  # dBm
-INTERFERER_MODE = "IEEE802154250Kbit"
-LENGTH_INTERFERER = 64  # Bytes
-
-tx_power_values = [8, 7, 6, 5, 4, 3, 2, 0, -4, -8, -12, -16, -20, -40]  # dBm
-radio_modes = ["BLE1MBit", "BLE2MBit", "BLELR125Kbit", "BLELR500Kbit", "IEEE802154250Kbit", "tone"]
+from config import (
+    DEFAULT_USB_PORT,
+    DEFAULT_BAUDRATE,
+    FREQ_INTERFERER,
+    TX_POWER_INTERFERER,
+    TX_POWER_LINK,
+    INTERFERER_MODE,
+    tx_power_values,
+    radio_modes,
+)
 
 
 # Class to handle serial communication between the gateway and the computer
