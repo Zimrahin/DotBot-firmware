b312b0fac42a2769bdf0e0d274fc1c06f5b6f018
Zimrahin <diegobadillosj@gmail.com>
2024-10-30 11:34:23 +0100

    changed PAYLOAD event to END event for debugging

--- projects/01bsp_radio_tx/01bsp_radio_tx.c
+++ projects/01bsp_radio_tx/01bsp_radio_tx.c
@@ -26,7 +26,7 @@
 
 #define MAX_PAYLOAD_SIZE (120)  // Maximum message size
 #define PPI_CH_ADDRESS   (0)    // PPI channel destined to radio address event debugging
-#define PPI_CH_PAYLOAD   (1)    // PPI channel destined to radio payload event debugging
+#define PPI_CH_END       (1)    // PPI channel destined to radio end event debugging
 #define GPIOTE_CH_OUT    (0)    // GPIOTE channel for RADIO TX
 
 typedef struct __attribute__((packed)) {
@@ -63,8 +63,8 @@ static _radio_pdu_t _radio_pdu = { 0 };
 //=========================== callbacks =========================================
 
 static void _tx_callback(void) {
-    db_gpio_toggle(&_dbg_pin);  // Toggle LED
     _tx_flag = true;
+    db_gpio_toggle(&_dbg_pin);  // Toggle LED
 }
 
 //=========================== functions =========================================
@@ -79,7 +79,7 @@ void _gpiote_setup(const gpio_t *gpio_e) {
 
 void _ppi_setup(void) {
     // Enable PPI channels
-    NRF_PPI->CHENSET = (1 << PPI_CH_ADDRESS) | (1 << PPI_CH_PAYLOAD);
+    NRF_PPI->CHENSET = (1 << PPI_CH_ADDRESS) | (1 << PPI_CH_END);
 
     // Set event and task endpoints for radio address event
     uint32_t radio_events_address   = (uint32_t)&NRF_RADIO->EVENTS_ADDRESS;
@@ -87,11 +87,11 @@ void _ppi_setup(void) {
     NRF_PPI->CH[PPI_CH_ADDRESS].EEP = radio_events_address;
     NRF_PPI->CH[PPI_CH_ADDRESS].TEP = gpiote_tasks_set;
 
-    // Set event and task endpoints for radio payload event
-    uint32_t radio_events_payload   = (uint32_t)&NRF_RADIO->EVENTS_PAYLOAD;
-    uint32_t gpiote_tasks_clr       = (uint32_t)&NRF_GPIOTE->TASKS_CLR[GPIOTE_CH_OUT];
-    NRF_PPI->CH[PPI_CH_PAYLOAD].EEP = radio_events_payload;
-    NRF_PPI->CH[PPI_CH_PAYLOAD].TEP = gpiote_tasks_clr;
+    // Set event and task endpoints for radio end event
+    uint32_t radio_events_end   = (uint32_t)&NRF_RADIO->EVENTS_END;
+    uint32_t gpiote_tasks_clr   = (uint32_t)&NRF_GPIOTE->TASKS_CLR[GPIOTE_CH_OUT];
+    NRF_PPI->CH[PPI_CH_END].EEP = radio_events_end;
+    NRF_PPI->CH[PPI_CH_END].TEP = gpiote_tasks_clr;
 }
 
 //=========================== main ==============================================
--- projects/01bsp_radio_tx/conf.h
+++ projects/01bsp_radio_tx/conf.h
@@ -2,6 +2,6 @@
 #include "radio.h"
 
 #define DOTBOT_GW_RADIO_MODE DB_RADIO_BLE_1MBit
-#define DELAY_MS             (1000)  // Wait DELAY_MSms between each send
+#define DELAY_MS             (1000)  // Wait DELAY_MS ms between each send
 #define FREQUENCY            (50)    // (2400 + FREQUENCY) MHz
 #define TIMER_DEV            (0)
