a8016d52d7dd5f286fe9073482d4ca383e276bd3
Zimrahin <diegobadillosj@gmail.com>
2024-10-31 18:58:35 +0100

    added condition for start of payload depending on if using 15.4 or BLE BLE includes 2 additional bytes before the payload, after the address, which correspond to S0 and length Still need to make the PPI and GPIOTE functions global for both master and slave transmitters

--- projects/01bsp_radio_tx/01bsp_radio_tx.c
+++ projects/01bsp_radio_tx/01bsp_radio_tx.c
@@ -69,29 +69,33 @@ static void _tx_callback(void) {
 
 //=========================== functions =========================================
 
-void _gpiote_setup(const gpio_t *gpio_e) {
+void _gpiote_setup(const gpio_t *gpio_pin) {
     NRF_GPIOTE->CONFIG[GPIOTE_CH_OUT] = (GPIOTE_CONFIG_MODE_Task << GPIOTE_CONFIG_MODE_Pos) |
-                                        (gpio_e->pin << GPIOTE_CONFIG_PSEL_Pos) |
-                                        (gpio_e->port << GPIOTE_CONFIG_PORT_Pos) |
+                                        (gpio_pin->pin << GPIOTE_CONFIG_PSEL_Pos) |
+                                        (gpio_pin->port << GPIOTE_CONFIG_PORT_Pos) |
                                         (GPIOTE_CONFIG_POLARITY_None << GPIOTE_CONFIG_POLARITY_Pos) |
                                         (GPIOTE_CONFIG_OUTINIT_Low << GPIOTE_CONFIG_OUTINIT_Pos);
 }
 
-void _ppi_setup(void) {
+void _ppi_setup(db_radio_mode_t mode) {
     // Enable PPI channels
     NRF_PPI->CHENSET = (1 << PPI_CH_ADDRESS) | (1 << PPI_CH_END);
 
     // Set event and task endpoints for radio address event
-    uint32_t radio_events_address   = (uint32_t)&NRF_RADIO->EVENTS_ADDRESS;
+    if (mode == DB_RADIO_IEEE802154_250Kbit) {
+        uint32_t radio_events_payload_start = (uint32_t)&NRF_RADIO->EVENTS_FRAMESTART;
+    } else {
+        uint32_t radio_events_payload_start = (uint32_t)&NRF_RADIO->EVENTS_ADDRESS;
+    }
     uint32_t gpiote_tasks_set       = (uint32_t)&NRF_GPIOTE->TASKS_SET[GPIOTE_CH_OUT];
-    NRF_PPI->CH[PPI_CH_ADDRESS].EEP = radio_events_address;
+    NRF_PPI->CH[PPI_CH_ADDRESS].EEP = radio_events_payload_start;
     NRF_PPI->CH[PPI_CH_ADDRESS].TEP = gpiote_tasks_set;
 
     // Set event and task endpoints for radio end event
-    uint32_t radio_events_end   = (uint32_t)&NRF_RADIO->EVENTS_END;
-    uint32_t gpiote_tasks_clr   = (uint32_t)&NRF_GPIOTE->TASKS_CLR[GPIOTE_CH_OUT];
-    NRF_PPI->CH[PPI_CH_END].EEP = radio_events_end;
-    NRF_PPI->CH[PPI_CH_END].TEP = gpiote_tasks_clr;
+    uint32_t radio_events_payload_end = (uint32_t)&NRF_RADIO->EVENTS_PAYLOAD;
+    uint32_t gpiote_tasks_clr         = (uint32_t)&NRF_GPIOTE->TASKS_CLR[GPIOTE_CH_OUT];
+    NRF_PPI->CH[PPI_CH_END].EEP       = radio_events_payload_end;
+    NRF_PPI->CH[PPI_CH_END].TEP       = gpiote_tasks_clr;
 }
 
 //=========================== main ==============================================
@@ -99,7 +103,7 @@ void _ppi_setup(void) {
 int main(void) {
     // Set PPI and GPIOTE
     _gpiote_setup(&_dbg_pin_ppi);
-    _ppi_setup();
+    _ppi_setup(DOTBOT_GW_RADIO_MODE);
 
     // Turn ON the DotBot board regulator
     db_board_init();
