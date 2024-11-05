82d1bc6b06b89f632507d90c4914bcfb30861bbc
Zimrahin <diegobadillosj@gmail.com>
2024-11-05 11:29:06 +0100

    added PHYEND and TXREADY events for debugging The blocker blocks from START, which is shorted to TXREADY, and ends at PHYEND (including the CRC)

--- projects/01bsp_radio_tx/conf.h
+++ projects/01bsp_radio_tx/conf.h
@@ -3,6 +3,6 @@
 
 #define DOTBOT_GW_RADIO_MODE DB_RADIO_BLE_1MBit
 #define TX_POWER             RADIO_TXPOWER_TXPOWER_0dBm  // PosXdBm, 0dBm, NegXdBm
-#define DELAY_MS             (1000)                      // Wait DELAY_MS ms between each send
+#define DELAY_MS             (400)                       // Wait DELAY_MS ms between each send
 #define FREQUENCY            (25)                        // (2400 + FREQUENCY) MHz
 #define TIMER_DEV            (0)
--- projects/01bsp_radio_tx/ppi_functions.h
+++ projects/01bsp_radio_tx/ppi_functions.h
@@ -9,10 +9,6 @@
 #include "gpio.h"
 #include "radio.h"
 
-#define PPI_CH_ADDRESS (0)  // PPI channel destined to radio address event debugging
-#define PPI_CH_END     (1)  // PPI channel destined to radio end event debugging
-#define GPIOTE_CH_OUT  (1)  // GPIOTE channel for RADIO TX
-
 // Function prototypes
 void _gpiote_setup(const gpio_t *gpio_pin);
 void _ppi_setup(db_radio_mode_t mode);
