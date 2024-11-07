6deac936a8089165bf26d476af0aa35c3e401b20
Zimrahin <diegobadillosj@gmail.com>
2024-11-07 16:17:20 +0100

    deleted radio_tx

--- projects/01bsp_radio_tx/conf.h
+++
@@ -1,8 +0,0 @@
-#include <nrf.h>
-#include "radio.h"
-
-#define DOTBOT_GW_RADIO_MODE DB_RADIO_BLE_1MBit
-#define TX_POWER             RADIO_TXPOWER_TXPOWER_0dBm  // PosXdBm, 0dBm, NegXdBm
-#define DELAY_MS             (400)                       // Wait DELAY_MS ms between each send
-#define FREQUENCY            (25)                        // (2400 + FREQUENCY) MHz
-#define TIMER_DEV            (0)
