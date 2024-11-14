eaf0e592ec738198b11e4f7a0bab6a1eb666cde7
Zimrahin <diegobadillosj@gmail.com>
2024-11-14 17:37:58 +0100

    settings for 15.4 txmain and BLE and tone blockers

--- projects/01bsp_radio_tone/conf.h
+++ projects/01bsp_radio_tone/conf.h
@@ -2,6 +2,6 @@
 #include "radio.h"
 
 #define TX_POWER  RADIO_TXPOWER_TXPOWER_0dBm  // PosXdBm, 0dBm, NegXdBm
-#define DELAY_us  (255)                       // Wait DELAY_us before sending
-#define SINE_us   (645)                       // Duration of sinusoidal wave
+#define DELAY_us  (940)                       // Wait DELAY_us before sending
+#define SINE_us   (1088)                      // Duration of sinusoidal wave
 #define FREQUENCY (25)                        // (2400 + FREQUENCY) MHz
