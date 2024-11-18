fc0f6b76a56538f7e216e9adee342cbb5ff91039
Zimrahin <diegobadillosj@gmail.com>
2024-11-18 15:35:41 +0100

    spelling

--- projects/01bsp_radio_tx_blocker/main.c
+++ projects/01bsp_radio_tx_blocker/main.c
@@ -2,7 +2,7 @@
  * @file
  * @ingroup samples_bsp
  * @author Diego Badillo-San-Juan <diego.badillo-san-juan@inria.fr>
- * @brief This example shows how to transmit synched interference packets over the radio
+ * @brief This example shows how to transmit synced interference packets over the radio
  *
  * @copyright Inria, 2024
  *
