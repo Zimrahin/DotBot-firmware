b6ddc7d604eb93064fe1676c726b3c531c98f8bb
Zimrahin <diegobadillosj@gmail.com>
2025-02-05 14:45:12 +0100

    packets for debugging SDR PHY demodulation implementation

--- projects/01bsp_radio_txrx_ieee802154/conf.h
+++ projects/01bsp_radio_txrx_ieee802154/conf.h
@@ -2,5 +2,5 @@
 #include "radio.h"
 
 #define DOTBOT_GW_RADIO_MODE DB_RADIO_IEEE802154_250Kbit
-#define DELAY_MS             (100)  // Wait 100ms between each send
-#define CHANNEL              (15)   // 2425 MHz for IEEE 802.15.4
+#define DELAY_MS             (6)   // Wait 100ms between each send
+#define FREQUENCY            (23)  // 2423 MHz for IEEE 802.15.4
